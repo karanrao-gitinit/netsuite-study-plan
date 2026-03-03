7ae6e6fe79806b6de991ec82dfc115c1a9e5dc9b
Karan Rao <129153519+karanrao-gitinit@users.noreply.github.com>
2026-03-03 18:06:17 +0530

    Update vite.config.js

--- netsuite-study-plan/vite.config.js
+++ netsuite-study-plan/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/karanrao-gitinit/netsuite-study-plan',
+  base: '/netsuite-study-plan/',
 })
